97932de9533dccf827567b5c28a1b0197cc9d567
sfilata <sfilata@hotmail.com>
2019-06-19 11:36:56 +0800

    #2 fix bug

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// this tests the app component is crashed
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
